2767339586b75d74e6641419b29a561d34b757e7
17871787 <jtowers349@gmail.com>
2025-09-07 18:13:40 +0100

    test: add frontpage and api endpoint tests

---
+++ app/__tests__/page.test.tsx
@@ -0,0 +1,27 @@
+/** @jest-environment jsdom */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Page from '../page';
+
+jest.mock('recharts', () => ({
+  ResponsiveContainer: ({ children }: any) => <div>{children}</div>,
+  LineChart: ({ children }: any) => <div>{children}</div>,
+  Line: () => <div />,
+  BarChart: ({ children }: any) => <div>{children}</div>,
+  Bar: () => <div />,
+  XAxis: () => <div />,
+  YAxis: () => <div />,
+  CartesianGrid: () => <div />,
+  Tooltip: () => <div />,
+  Legend: () => <div />,
+  PieChart: ({ children }: any) => <div>{children}</div>,
+  Pie: ({ children }: any) => <div>{children}</div>,
+  Cell: () => <div />,
+}));
+
+describe('Dashboard Page', () => {
+  it('renders heading', () => {
+    render(<Page />);
+    expect(screen.getByText('AM Copilot')).toBeTruthy();
+  });
+});
